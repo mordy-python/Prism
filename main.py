1031cdeed5ca5164d12a8411dc6cb55ce332e95c
mordy <imky171@gmail.com>
2022-07-02 10:09:36 -0400

    Started adding styles

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ users = deta.Base('users')
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
