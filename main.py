0515f337ea1927e4b024b49d9609798a0808bfa1
mordy <imky171@gmail.com>
2022-07-01 21:57:57 -0400

    main commit

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from deta import Deta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+deta = Deta()
+users = deta.Base('users')
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
